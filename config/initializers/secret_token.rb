07493a8803198e88f0525c0f2a7d075d1290669c
yutaro <yutaro.tanaka.world@gmail.com>
2015-02-25 20:56:37 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StiSample::Application.config.secret_key_base = 'c62df8a325777e0778cd595b0a320fa39f4c52a5c4e323f37f5dadc1d05df83833cfe3bf0088e0f4c6021be652bec6596b099073de02809dc8c206e0fa04029c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StiSample::Application.config.session_store :cookie_store, key: '_sti_sample_session'
